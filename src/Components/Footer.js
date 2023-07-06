bfa51ab56b2a2e20208f8c154b7c90ee7eac64ac
unknown <pkpalash237.com>
2023-07-06 22:50:02 +0600

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+  return(
+    <footer className="bg-dark text-light my-4 py-2" >
+      <p  className="text-center">
+        Copyright &copy; Palash Kumer
+      </p>
+      </footer>
+
+  ) 
+}
